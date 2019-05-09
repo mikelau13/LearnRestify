6344f6f872e91bc38c28b4b0aede7a11dac5ccf7
Mike Lau <mlau@metroland.com>
2019-05-09 11:33:06 -0400

    Hypermedia

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 npm install restify
 
 npm start
+
+
+http://localhost:8080/foo?a=b
+
+Hypermedia
+http://localhost:8080/cities
+http://localhost:8080/anotherroute
--- app.js
+++ app.js
@@ -21,6 +21,10 @@ server.get('/', function(req, res, next) {
     return next();
   });
 server.head('/hello/:name', respond);
+server.del('/hello/:name', function rm(req, res, next) {
+    res.send(204);
+    return next();
+  });
 server.get('/foo:a',
   function(req, res, next) {
     req.someData = 'foo' + req.params.a;
@@ -30,6 +34,32 @@ server.get('/foo:a',
     res.send(req.someData);
     return next();
   }
+);
+
+// server.get(/^\/([a-zA-Z0-9_\.~-]+)\/(.*)/, function(req, res, next) {
+//   console.log(req.params[0]);
+//   console.log(req.params[1]);
+//   res.send(200);
+//   return next();
+// });
+
+
+// Hypermedia
+server.get({name: 'city', path: '/cities/:slug'},
+  function (req, res, next) {
+    res.send('city is ' + req.params.slug);
+    return next();
+  }
+);
+
+server.get('/anotherroute',
+    function (req, res, next) {
+        res.send({
+            country: 'Australia',
+            // render a URL by specifying the route name and parameters
+            capital: server.router.render('city', {slug: 'canberra'}, {details: true})
+        })
+    }
 );
 
 server.listen(8080, function() {
